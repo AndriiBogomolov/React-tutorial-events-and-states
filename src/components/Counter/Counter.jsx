996ad388766c6bf2525c158d9d4f097cecec2271
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-01-18 00:15:14 +0100

    css upd

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Controls from './Controls';
 import './Counter.css';
 
 class Counter extends React.Component {
@@ -8,13 +9,13 @@ class Counter extends React.Component {
 
     handleIncrement = () => {
         this.setState(prevState => ({
-            value: prevState.value + 2,
+            value: prevState.value + 1,
         }));
     };
 
     handleDecrement = () => {
         this.setState(prevState => ({
-            value: prevState.value - 2,
+            value: prevState.value - 1,
         }));
     };
 
@@ -23,10 +24,16 @@ class Counter extends React.Component {
             <div className='Counter'>
                 <span className='Counter__value'>{this.state.value}</span>
 
-                <div className='Counter__controls'>
+                <Controls
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                />
+            
+
+                {/* <div className='Counter__controls'>
                     <button type='button' onClick={this.handleIncrement}>Увеличить на 1</button>
                     <button type='button' onClick={this.handleDecrement}>Уменьшить на 1</button>
-                </div>
+                </div> */}
             </div>
         );
     }
