9d940de0ee8d304318396071ce3ca4f1e990ec98
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-01-17 16:25:59 +0100

    2d upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Counter from './Counter/Counter.jsx';
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#f44336' },
+//   { label: 'green', color: '#4caf50' },
+//   { label: 'blue', color: '#2196f3' },
+// ];
+
+export const App = () => (
+  <>
+  <h1>Состояние компонента</h1>
+  
+  <Counter/>
+
+  </>
+  // return (
+  //   <div
+  //     style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
+  //   >
+  //     React homework template
+  //   </div>
+  // );
+);
+// export default App;
